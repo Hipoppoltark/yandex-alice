7529a0bb50e57ee628761ab410fbbbd2673661e0
Hipoppoltark <48064845+Hipoppoltark@users.noreply.github.com>
2021-04-21 21:58:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,6 @@ def handle_dialog(res, req):
                                           f'Сыграем еще?'
                 sessionStorage[user_id]['now_city'] = None
                 sessionStorage[user_id]['start_game'] = False
-                sessionStorage[user_id]['guessed_city'] = []
                 sessionStorage[user_id]['images_for_show'] = []
                 res['response']['buttons'] = [
                     {
@@ -180,6 +179,8 @@ def handle_dialog(res, req):
                 return
             res['response']['text'] = 'Правильно. Сыграем еще?'
             sessionStorage[user_id]['start_game'] = False
+            sessionStorage[user_id]['now_city'] = None
+            sessionStorage[user_id]['images_for_show'] = []
             res['response']['buttons'] = [
                 {
                     'title': 'Да',
